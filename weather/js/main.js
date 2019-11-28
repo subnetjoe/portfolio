73330b746686916cd8a7d9a7b94445efb126ca5c
William Early <william.joe.early@gmail.com>
2019-11-27 21:49:45 -0500

    Add files via upload

---
+++ weather/js/main.js
@@ -0,0 +1,16 @@
+
+var city = "New York";
+
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=a4f1e1fffd21c3ab5f84c30ea39c7a47", 
+    function(data){
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = Math.floor(data.main.temp);
+    var weather = data.weather[0].main;
+
+    $(".icon").attr('src', icon);
+    $(".weather").append(weather);
+    $(".temp").append(temp);
+    }
+);
