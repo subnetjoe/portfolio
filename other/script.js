66519e5b554b8158070342234e160bbb84ea34b1
subnetjoe <william.joe.early@gmail.com>
2021-12-13 21:08:38 -0500

    getting it

--- other/script.js
+++ other/script.js
@@ -1,1 +1,12 @@
-console.log('Hello Joe');
+let food = [
+    'soup',
+    'pancakes'
+];
+
+const creditBuilder = 463.86 + 122.77;
+
+console.log(food);
+
+document.getElementById("demo").innerHTML = creditBuilder
+
+// document.write(creditBuilder);
