57fb262d3303e205f5cc4ee166d0818c2a7d72c3
subnetjoe <william.joe.early@gmail.com>
2021-12-13 23:13:51 -0500

    n

--- other/script.js
+++ other/script.js
@@ -3,12 +3,12 @@ let food = [
     'pancakes'
 ];
 
-const creditBuilder = 463.86 + 122.77 + 133.23;
-const monthlySupport = creditBuilder - 545;
+const creditBuilder = 463.86 + 122.77 + 133.23 + 154.44;
+const monthlySupport = creditBuilder - 545.00;
 console.log(food);
 
-document.getElementById("december").innerHTML = '$' + creditBuilder
-document.getElementById("january").innerHTML = '$' + monthlySupport
+document.getElementById("december").innerHTML = '$' + creditBuilder.toFixed(2)
+document.getElementById("january").innerHTML = '$' + monthlySupport.toFixed(2)
 
 // document.write(creditBuilder);
 //new
