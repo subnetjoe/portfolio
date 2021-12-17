f8a1c9defc14d861e1867e547b45e2c8d25579c5
subnetjoe <william.joe.early@gmail.com>
2021-12-16 19:07:16 -0500

    new commit

--- budget/js/app.js
+++ budget/js/app.js
@@ -16,12 +16,34 @@ class UI {
         this.itemList = [];
         this.itemID = 0;
     }
+    //submit budget method
+    submitBudgetForm(){
+        console.log('hello from es6');
+    }
 }
 
 function eventListeners(){
 const budgetForm = document.getElementById('budget-form');
 const expenseForm = document.getElementById('expense-form');
 const expenseList = document.getElementById('expense-list');
+
+// new instance of UI CLASS
+const ui = new UI()
+
+// budget form submit
+budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+})
+// expense form submit
+expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+})
+// expense click
+expenseList.addEventListener('click', function(){
+    
+})
+
 }
 
 document.addEventListener('DOMContentLoaded', function(){
