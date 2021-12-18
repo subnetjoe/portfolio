c3c0ac152d4ec6905e5533d6f9ebf4814c7b1037
subnetjoe <william.joe.early@gmail.com>
2021-12-18 15:08:29 -0500

    new commit

--- javascript/menu/app.js
+++ javascript/menu/app.js
@@ -1,3 +1,4 @@
+// items
 const menu = [
   {
     id: 1,
@@ -72,3 +73,7 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+
+
+
