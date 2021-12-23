b9dc3a0163114b9ca6421e063683483ff47e8b4f
subnetjoe <william.joe.early@gmail.com>
2021-12-23 16:21:26 -0500

    new commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,13 @@
-const kern = ['joe', 'john', 'tim'];
-
-console.table(kern);
+function mapString(string){
+  let map = {
+  };
+  for (let i = 0; i < string.length; i++){
+    let letter = string[i];
+    if(map[letter]){
+      map[letter].push(i);
+    } else{
+      map[letter] = [i];
+    }
+  }
+  return map;
+};
