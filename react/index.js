b8f25dab4ce4b2ad3848465631d3a0fbeea961e6
subnetjoe <william.joe.early@gmail.com>
2022-01-11 17:15:01 -0500

    new

--- react/index.js
+++ react/index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+const page = (
+  <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
