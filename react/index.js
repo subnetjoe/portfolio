6b20d923e71e8cd4cdfab9ef9786cfca70f337cf
subnetjoe <william.joe.early@gmail.com>
2022-01-11 17:26:59 -0500

    new

--- react/index.js
+++ react/index.js
@@ -1,12 +1,17 @@
+
+
 const page = (
-  <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+  <div>
+    <img src="./react-logo.png" width="40px"/>
+    <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </div>
 )
 
 ReactDOM.render(page, document.getElementById("root"))
