e268fb428a006be777feee7cab726b06515acd0d
subnetjoe <william.joe.early@gmail.com>
2022-01-12 12:35:56 -0500

    new

--- react/index.js
+++ react/index.js
@@ -1,5 +1,5 @@
-import React from "React"
-import ReactDOM from "React-dom"
+import React from "react"
+import ReactDOM from "react-dom"
 import Header from "./Header"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
