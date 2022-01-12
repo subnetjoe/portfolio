8f8f26e22a64c1a8b03200b8a587fc692e484b0a
subnetjoe <william.joe.early@gmail.com>
2022-01-12 12:04:56 -0500

    new

--- react/index.js
+++ react/index.js
@@ -1,17 +1,50 @@
+function Header(){
+  return(
+    <div>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width="40px"/>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+function MainContent(){
+  return(
+    <div>
+      <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+          </ol>
+    </div>
+  )
+}
+
+function Footer(){
+  return(
+    <div>
+      <footer>
+        <small>c 2022 Early development. All rights reserved.</small>
+      </footer>
+    </div>
+  )
+}
 
 
-const page = (
-  <div>
-    <img src="./react-logo.png" width="40px"/>
-    <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
   </div>
-)
+  )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+
+ReactDOM.render(<Page />, document.getElementById("root"))
