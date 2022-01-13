1d1c0390d6f548659ef6fdb0ee905f1576ffcb47
subnetjoe <william.joe.early@gmail.com>
2022-01-13 13:04:33 -0500

    new

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,11 +1,11 @@
-import logo from "./react-logo.png"
+import "./react-logo.png"
 
 export default function Header(){
   return(
     <div>
       <header>
         <nav className="nav">
-          <img src="./react-logo.png" className="logo" alt="" />
+          <img src="./react-logo.png" className="nav-logo" alt="" />
           <ul className="nav-items">
             <li>Pricing</li>
             <li>About</li>
