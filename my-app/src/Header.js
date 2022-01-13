7e452adfd2e93fdcb8183cfb430c2bd6cbbe974d
subnetjoe <william.joe.early@gmail.com>
2022-01-13 13:22:00 -0500

    react

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,11 +1,11 @@
-import './react-logo.png';
+import Image from "./react-logo.png";
 
 export default function Header(){
   return(
     <div>
       <header>
         <nav className="nav">
-          <img src="./react-logo.png" className="nav-logo" alt="" />
+          <img src={Image} className="nav-logo" alt="Tag" />
           <ul className="nav-items">
             <li>Pricing</li>
             <li>About</li>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index2.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './react-logo.png';
