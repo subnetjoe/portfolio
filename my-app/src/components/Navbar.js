e60678d0113039dc282f9a1c615005010f7c1390
subnetjoe <william.joe.early@gmail.com>
2022-01-14 18:04:38 -0500

    react

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(){
   return(
     <nav>
       <img src={Logo} tag="React Logo" alt="React Logo" className="nav--icon"></img>
-      <h3 className="nav--logo_text">Reactfacts</h3>
+      <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   )
