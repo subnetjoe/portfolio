bd6df1e41a3732882fed06ba094054f2801f213e
subnetjoe <william.joe.early@gmail.com>
2022-04-12 21:49:23 -0400

    new

---
+++ js/Sort.js
@@ -0,0 +1,10 @@
+const data = ["Banana", "Orange", "Apple", "Mango"];
+
+//Standard method of Sort
+const sort = data.sort()
+console.log(sort);
+
+//Sorting Z-A
+const sort2 = data.sort().reverse()
+console.log(sort2);
+
