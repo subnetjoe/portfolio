e17741785176ef69a4e8f74ea6a446831f9f847b
subnetjoe <william.joe.early@gmail.com>
2022-04-23 14:30:54 -0400

    load

--- portfolio/preload.js
+++ portfolio/preload.js
@@ -2,4 +2,4 @@ let spinnerWrapper = document.querySelector('.spinner-wrapper');
   window.addEventListener('load', function () {
     spinnerWrapper.style.display = 'none';
     // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-  })
+  });
