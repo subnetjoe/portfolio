089a8d73fdc1b9f6c676db310f5fe80981ced532
Joe Early <william.joe.early@gmail.com>
2022-05-12 19:40:42 -0400

    and

--- double/script.js
+++ double/script.js
@@ -1,34 +1,22 @@
 //SELECTORS 
-const button = document.querySelector("button");
+const whatButton = document.querySelector('.double-click');
 
-//TRIGGERS + LISTENERS
-button.addEventListener('click', e => {
-    console.log('Double Click');
+//TRIGGERS + EVENT LISTENERS
+
+whatButton.addEventListener('click', function e() {
+    console.log(e.currentTarget);
 });
 
-// const MAX_DOUBLE_CLICK_TIME = 500
-// let lastClick = 0
-// button.addEventListener("click", e => {
-//     const timeBetweenClicks = e.timeStamp - lastClick
-//     if (timeBetweenClicks > MAX_DOUBLE_CLICK_TIME){
-//         lastClick = e.timeStamp
-//         return
-//     }
-
-// const doubleClickEvent = new CustomEvent("custom:doubleClick", {
-//     bubbles: true,
-//     cancelable: true,
-//     composed: true,
-//     detail:{
-//         timeBetweenClicks,
-//     },
-// })
-// e.target.dispatchEvent(doubleClickEvent)
-// lastClick = 0
+// document.addEventListener('DOMContentLoaded', function () {
+//     whatButton.addEventListener('click', e => {
+//         console.log(e.currentTarget);
+//     });
 // });
 
-button.dispatchEvent(myEvent)
-
+// var el = document.getElementById('double-click');
+// if(el){
+//   el.addEventListener('click', e =>);
+// }
 
 //FUNCTIONS
 
