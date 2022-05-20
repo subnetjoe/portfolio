39d4a5d1901d4e889e67f2ac51f420da60f93720
Joe Early <william.joe.early@gmail.com>
2022-05-20 16:05:53 -0400

    working

--- javascript/Web/script.js
+++ javascript/Web/script.js
@@ -1,14 +1,27 @@
 //SELECTORS
 const element = document.querySelector('.Js-icon');
+const elementIdeas = document.querySelector('.jsfunctions');
 
+
+//addEventListeners need to be in class methods and not ID
 //ACTION EVENT HANDLERS
-// document.getElementById('Js-icon').addEventListener('click', jsIdeas);
+element.addEventListener('click', jsIdeas);
 
 //FUNCTIONS
+function jsIdeas(){
+    if(elementIdeas.style.display == 'block'){
+        elementIdeas.style.display = 'none';
+    } else{
+        elementIdeas.style.display = 'block';
+    }
+};
 
-element.addEventListener('click', (event) => {
-    console.log('HI!', event.target);
-});
+
+
+
+// element.addEventListener('click', (event) => {
+//     console.log('HI!', event.target);
+// });
 
 
 
