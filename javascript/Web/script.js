41eff1efd80c25b9d22bb177a360e704aa230f1b
Joe Early <william.joe.early@gmail.com>
2022-05-20 12:44:15 -0400

    javascript

---
+++ javascript/Web/script.js
@@ -0,0 +1,17 @@
+//SELECTORS
+const jsIcon = document.querySelector('Js-icon');
+
+//ACTION EVENT HANDLERS
+jsIcon.addEventListener('click', jsIdeas);
+
+//FUNCTIONS
+function jsIdeas(){
+    console.log(jsIdeas);
+    document.getElementById("jsfunctions").style.display = "none";
+};
+
+
+
+
+
+
