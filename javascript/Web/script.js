63648643285a4e19b2eb2d42809af379a2cc8c1b
Joe Early <william.joe.early@gmail.com>
2022-05-20 14:23:42 -0400

    good

--- javascript/Web/script.js
+++ javascript/Web/script.js
@@ -1,24 +1,15 @@
 //SELECTORS
-const jsIcon = document.querySelector('Js-icon');
+const div = document.querySelector('Js-icon');
 const jsFunctions = document.querySelector('jsfunctions');
 
 //ACTION EVENT HANDLERS
-document.addEventListener('click', jsIdeas);
+// document.getElementById('Js-icon').addEventListener('click', jsIdeas);
 
 //FUNCTIONS
 
-function jsIdeas(){
-    const jsFunctions = document.style.display ;
-};
-
-
-
-// .addEventListener('click', function jsIdeas(){
-//     if (document.getElementById('jsfunctions').style.display == 'none')
-//         {return false;}
-//         return true;}
-// );
-
+window.addEventListener('click', (event) => {
+    console.log('HI!', event.target);
+});
 
 
 
