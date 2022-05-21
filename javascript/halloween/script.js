3bea56e90048d57ef4a669d7c58df0db392646a7
Joe Early <william.joe.early@gmail.com>
2022-05-21 11:52:18 -0400

    halloween

---
+++ javascript/halloween/script.js
@@ -0,0 +1,15 @@
+// SELECTORS
+// const clickMe = document.querySelector('button');
+
+// // EVENT HANDLERS
+// button.addEventListener('click', function)
+
+// FUNCTIONS
+
+
+
+
+let x = document.querySelector('button');
+document.querySelector('.Js-icon').onclick = function(){
+    x.classList.toggle('fade');
+}
