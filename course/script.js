34b325358a58211089573bfbb01e1def0ea1e2e8
Joe Early <william.joe.early@gmail.com>
2022-05-31 13:37:22 -0400

    temps

--- course/script.js
+++ course/script.js
@@ -96,14 +96,14 @@
 ////////////////////////////////setTimeout and setInterval(() =>)
 
 //set timeout
-const ingredients = ["olives", "spinach"];
+// const ingredients = ["olives", "spinach"];
 
-setTimeout(
-  (ing1, ing2) => console.log(`Your pizza with ${ing1} and ${ing2} 🍕`),
-  3000,
-  ...ingredients
-);
-console.log("Waiting...");
+// setTimeout(
+//   (ing1, ing2) => console.log(`Your pizza with ${ing1} and ${ing2} 🍕`),
+//   3000,
+//   ...ingredients
+// );
+// console.log("Waiting...");
 
 //setInterval(() =>) applies the function every 3 seconds or specified time
 
@@ -113,6 +113,73 @@ console.log("Waiting...");
 //     console.log(now);
 // }, 3000)
 
-const x = 23;
+////////////////////////////////////////////////////////////////////////////////////////////////
+//Find the max of an array
+
+// const calcTempAmplitude = function(temps){
+//   let max = temps[0];
+//   for (let i = 0; i < temps.length; i++){
+//     if(temps[i] > max) max = temps[i];
+//   }
+//   console.log(max);
+// };
+
+// calcTempAmplitude([3, 6, 9]);
+
+// //Find the min of an Array
+
+// const calcTempAmplitude2 = function(temps) {
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++){
+//     if(temps[i] < min) min = temps[i];
+//   }
+//   console.log(min);
+// };
+
+// calcTempAmplitude2([41, 18, 72]);
+
+//Find the max and min of an Array
+
+// const calcTempAmplitude = function (temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitude = calcTempAmplitude([3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]);
+
+// console.log(amplitude);
+
+////Find the max and min of an Array plus join two arrays together
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeNew([3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5], 
+[3, -62, -36, -1, 'error', 9, 13, 17, 65, 14, 91, 57]);
+
+console.log(amplitudeNew);
 
 
