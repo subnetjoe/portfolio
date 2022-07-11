9c4f5f7a837b2ddf2bf29e6e343c8d1ebc885899
Joe Early <william.joe.early@gmail.com>
2022-07-11 18:05:57 -0400

    animations

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// gsap.from("#logo", {duration: 1, y: '-150%', ease: 'bounce', opacity: 0});
+// gsap.from(".fab", {duration: 1, y: -100, opacity: 0, delay: 1.5});
+
+//Start a timeline
+
+const timeline = gsap.timeline({defaults: {duration: 1}});
+timeline
+    .from("#logo", {duration: 1, y: '-150%', ease: 'bounce', opacity: 0})
+    .from(".fab", {duration: 1, y: -100, opacity: 0})
+
+
+console.clear();
+let targets = gsap.utils.toArray("#logo");
+
+targets.forEach((obj) => {
+  obj.anim = gsap.to(obj, { height: 200 }).reversed(true);
+  obj.addEventListener("click", doCoolStuff);
+});
+
+function doCoolStuff() {
+  this.anim.reversed(!this.anim.reversed());
+}
