09c3303173b38cf13eb4a4f7ac9c58761eb849da
Joe Early <william.joe.early@gmail.com>
2022-07-13 21:01:22 -0400

    animation

--- js/script.js
+++ js/script.js
@@ -8,18 +8,24 @@ gsap.registerPlugin(ScrollTrigger);
 const timeline = gsap.timeline({defaults: {duration: 1}});
 timeline
     .from("#logo", {duration: 1, y: '-150%', ease: 'bounce', opacity: 0})
-    .from(".html5", {duration: 1, y: -100, opacity: 0})
-    .from(".css3", {duration: 1, y: -100, opacity: 0})
-    .from(".jScript", {duration: 1, y: -100, opacity: 0})
+    .from(".intro", {duration: 1, x: '-100%', y: '-100%', opacity: 0 })
+    .from(".html5", {duration: 1, y: '-100%', opacity: 0})
+    .from(".css3", {duration: 1, y: '-100%', opacity: 0})
+    .from(".jScript", {duration: 1, y: '-100%', opacity: 0})
 
-console.clear();
-let targets = gsap.utils.toArray("#logo");
 
-targets.forEach((obj) => {
-  obj.anim = gsap.to(obj, { height: 200 }).reversed(true);
-  obj.addEventListener("click", doCoolStuff);
-});
 
-function doCoolStuff() {
-  this.anim.reversed(!this.anim.reversed());
-}
+
+    
+    // click div to do and reverse animation
+// console.clear();
+// let targets = gsap.utils.toArray("#logo");
+
+// targets.forEach((obj) => {
+//   obj.anim = gsap.to(obj, { height: 200 }).reversed(true);
+//   obj.addEventListener("click", doCoolStuff);
+// });
+
+// function doCoolStuff() {
+//   this.anim.reversed(!this.anim.reversed());
+// }
