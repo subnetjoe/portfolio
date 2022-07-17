793fda756578b662bcd49b76d3d39341df3655d6
Joe Early <william.joe.early@gmail.com>
2022-07-16 20:40:43 -0400

    animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 // gsap.from("#logo", {duration: 1, y: '-150%', ease: 'bounce', opacity: 0});
 // gsap.from(".fab", {duration: 1, y: -100, opacity: 0, delay: 1.5});
 
@@ -23,72 +21,74 @@ timeline
   })
   .from(".html5", {
     scrollTrigger: {
-        trigger: ".html5",
-        start: "top center",
-      //   markers: true,
-        // id: "html5",
-      },
+      trigger: ".html5",
+      start: "top center",
+      // markers: true,
+      id: "html5",
+    },
     duration: 1,
     y: "-100%",
     opacity: 0,
+    ease: "power4.out",
   })
   .from(".css3", {
     scrollTrigger: {
-        trigger: ".css3",
-        start: "top center",
+      trigger: ".css3",
+      start: "top center",
       //   markers: true,
-        // id: "css3",
-      },
+      // id: "css3",
+    },
     duration: 1,
     y: "-100%",
     opacity: 0,
+    ease: "power4.out",
   })
   .from(".jScript", {
     scrollTrigger: {
-        trigger: ".jScript",
-        start: "top center",
+      trigger: ".jScript",
+      start: "top center",
       //   markers: true,
-        // id: "jScript",
-      },
+      // id: "jScript",
+    },
     duration: 1,
     y: "-100%",
     opacity: 0,
+    ease: "power4.out",
   });
 
 // click div to do and reverse animation
 // console.clear();
-// let targets = gsap.utils.toArray("#logo");
+let targets = gsap.utils.toArray("#logo");
 
-// targets.forEach((obj) => {
-//   obj.anim = gsap.to(obj, { height: 200 }).reversed(true);
-//   obj.addEventListener("click", doCoolStuff);
-// });
+targets.forEach((obj) => {
+  obj.anim = gsap.to(obj, { x: 300 }).reversed(true);
+  obj.addEventListener("click", doCoolStuff);
+});
 
-// function doCoolStuff() {
-//   this.anim.reversed(!this.anim.reversed());
-// }
+function doCoolStuff() {
+  this.anim.reversed(!this.anim.reversed());
+}
 
 gsap.from(".portfolio", {
-    scrollTrigger: {
-      trigger: ".portfolio",
-      start: "top center",
+  scrollTrigger: {
+    trigger: ".portfolio",
+    start: "top center",
     //   markers: true,
-      id: "portfolio",
-    },
-    x: -100,
-    opacity: 0,
-    duration: 1,
-  });
+    id: "portfolio",
+  },
+  x: -100,
+  opacity: 0,
+  duration: 1,
+});
 
-  gsap.from(".contact", {
-    scrollTrigger: {
-      trigger: ".contact",
-      start: "top center",
+gsap.from(".contact", {
+  scrollTrigger: {
+    trigger: ".contact",
+    start: "top center",
     //   markers: true,
-      id: "contact",
-    },
-    x: -100,
-    opacity: 0,
-    duration: 1,
-  });
-
+    id: "contact",
+  },
+  x: -100,
+  opacity: 0,
+  duration: 1,
+});
