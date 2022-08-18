476f9ef7b1bbb87723387ca576d6dc0f410068f7
Joe Early <william.joe.early@gmail.com>
2022-08-18 19:47:17 -0400

    still working on the menu

--- Projects/Garibaldi/js/main.js
+++ Projects/Garibaldi/js/main.js
@@ -14,6 +14,13 @@
     }
   }
 
+
+  const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
   /**
    * Easy event listener function
    */
