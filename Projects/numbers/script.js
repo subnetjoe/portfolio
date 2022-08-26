a69d36dc285894b4facaac6abad445139d130f7f
Joe Early <william.joe.early@gmail.com>
2022-08-26 12:54:24 -0400

    command

---
+++ Projects/numbers/script.js
@@ -0,0 +1,44 @@
+// Fetch() method: It is defined on th window object, which we can use to perform request.
+// This method will return a promise that will be fulfilled when the request is completed
+// or rejected if the request is rejected.
+
+let enter_btn = document.querySelector(".btn");
+let card = document.querySelector(".card");
+
+enter_btn.addEventListener("click", getFacts);
+
+function getFacts() {
+    let input = document.querySelector(".input").value;
+    let card_title = document.querySelector(".card-title");
+    let card_text = document.querySelector(".card-text");
+
+    if (input !== "") {
+        card.classList.remove("d-none");
+
+        fetch(`http://numbersapi.com/${input}`)
+            .then((response) => response.text())
+            .then((data) => {
+                card_title.innerHTML = input;
+                card_text.innerHTML = data;
+            })
+            .catch((err) => console.log(err));
+        }
+    }
+
+let copy_btn = document.querySelector(".copy-btn");
+
+copy_btn.addEventListener('click', () => {
+    copyText();
+});
+
+function copyText() {
+    let textarea = document.createElement("textarea");
+    let card_text = document.querySelector(".card-text").innerHTML;
+
+    textarea.value = card_text;
+    document.body.append(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Fact has been copied");
+};
