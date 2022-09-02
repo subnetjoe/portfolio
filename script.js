bfaa4aa5c93ccc901fec0070d37ce544068df896
Joe Early <william.joe.early@gmail.com>
2022-09-02 12:09:34 -0400

    updating

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
-
 const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { 
-        bsCollapse.toggle() 
+navLinks.forEach((el) => {
+    el.addEventListener('click', () => { 
+    bsCollapse.toggle();
     })
 })
 
